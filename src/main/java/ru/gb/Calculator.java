caf794c6771129167cfa9fcb716c0c93894f0d31
YuSTerekhina <loneliness2004@mail.ru>
2021-11-09 16:49:58 +0300

    Homework done

--- src/main/java/ru/gb/Calculator.java
+++ src/main/java/ru/gb/Calculator.java
@@ -1,56 +1,69 @@
 package ru.gb;
 
-import javax.swing.*;
+
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-public class Calculator extends JFrame {
-    public Calculator () {
-        setTitle("Calculator");
-        getContentPane().add(new CalculatorPanel());
-    }
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Calculator {
     public static void main(String[] args) {
-        Calculator frame = new Calculator ();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setBounds(400,200,200,200);
-        frame.setVisible(true);
+        EventQueue.invokeLater(new Runnable()
+        {
+            public void run() {
+                CalculatorFrame frame = new CalculatorFrame();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setBounds(400,200,300,400);
+                frame.setVisible(true);
+            }
+        });
+    }
+}
+class CalculatorFrame extends JFrame {
+    public CalculatorFrame() {
+        setTitle("Calculator");
+        CalculatorPanel panel = new CalculatorPanel();
+        add(panel);
+        pack();
     }
 }
 class CalculatorPanel extends JPanel {
-    private JLabel display;
-    private JPanel panel;
-    private double result;
-    private String lastCommand;
-    private boolean start;
     public CalculatorPanel() {
         setLayout(new BorderLayout());
+
         result = 0;
         lastCommand = "=";
-        start = true;
-        display = new JLabel("0");
+        start=true;
+
+        display = new JButton("0");
+        display.setEnabled(false);
         add(display, BorderLayout.NORTH);
+
         ActionListener insert = new InsertAction();
         ActionListener command = new CommandAction();
-        ActionListener clear = new ClearAction();
+
         panel = new JPanel();
-        panel.setLayout(new GridLayout(5, 4));
+        panel.setLayout(new GridLayout(4, 4));
+
         addButton("7", insert);
         addButton("8", insert);
         addButton("9", insert);
         addButton("/", command);
+
         addButton("4", insert);
         addButton("5", insert);
         addButton("6", insert);
         addButton("*", command);
+
         addButton("1", insert);
         addButton("2", insert);
         addButton("3", insert);
         addButton("-", command);
+
         addButton("0", insert);
         addButton(".", insert);
         addButton("=", command);
         addButton("+", command);
-        addButton("C", clear);
+
         add(panel, BorderLayout.CENTER);
     }
     private void addButton(String label, ActionListener listener) {
@@ -61,45 +74,40 @@ class CalculatorPanel extends JPanel {
     private class InsertAction implements ActionListener {
         public void actionPerformed(ActionEvent event) {
             String input = event.getActionCommand();
-            if (start) {
+            if(start) {
                 display.setText("");
                 start = false;
             }
             display.setText(display.getText() + input);
         }
     }
-    private class ClearAction implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            display.setText("0");
-        }
-    }
     private class CommandAction implements ActionListener {
-        public void actionPerformed(ActionEvent evt) {
-            String command = evt.getActionCommand();
-            if (start) {
-                if (command.equals("-")) {
+        public void actionPerformed(ActionEvent event) {
+            String command = event.getActionCommand();
+            if(start) {
+                if(command.equals("-")) {
                     display.setText(command);
                     start = false;
-                } else
-                    lastCommand = command;
-            } else {
+                } else lastCommand = command;
+            } else{
                 calculate(Double.parseDouble(display.getText()));
                 lastCommand = command;
-                start = true;
+                start=true;
             }
         }
     }
-    public void calculate(double x) {
-        if (lastCommand.equals("+"))
-            result += x;
-        else if (lastCommand.equals("-"))
-            result -= x;
-        else if (lastCommand.equals("*"))
-            result *= x;
-        else if (lastCommand.equals("/"))
-            result /= x;
-        else if (lastCommand.equals("="))
-            result = x;
+    public void calculate(double x)
+    {
+        if(lastCommand.equals("+")) result += x;
+        else if(lastCommand.equals("-")) result -= x;
+        else if(lastCommand.equals("*")) result *= x;
+        else if(lastCommand.equals("/")) result /= x;
+        else if(lastCommand.equals("=")) result = x;
         display.setText("" + result);
     }
+    private JButton display;
+    private JPanel panel;
+    private double result;
+    private String lastCommand;
+    private boolean start;
 }
